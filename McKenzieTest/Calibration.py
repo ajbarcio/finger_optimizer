9bcad7ed26c857ae79eb086153618e701eea3d7c
mckenzie-mccain <mccainmckenzie@gmail.com>
2026-08-06 09:30:44 -0500

    the jacobian works >:)

--- McKenzieTest/Calibration.py
+++ McKenzieTest/Calibration.py
@@ -2,74 +2,125 @@ import numpy as np
 from scipy.differentiate import jacobian
 import sympy as sp
 
-def get_bifurcation_angles(target_prox_T1, target_prox_T2, target_term_T1, target_term_T2, prox_slip, term_slip, prop_prox):
-    # target_prox_T1 = -2.174503356588717; target_prox_T2 = -2.418878165370875
-    # target_term_T1 = -0.562503356588717; target_term_T2 = -1.650078165370875
+def get_bifurcation_angles(target_prox_t2, target_prox_t3, target_term_t2, target_term_t3, prox_slip, term_slip, prop_prox):
+    # target_prox_t2 = -2.174503356588717; target_prox_t3 = -2.418878165370875
+    # target_term_t2 = -0.562503356588717; target_term_t3 = -1.650078165370875
     # prox_slip = -3.479205370541947; term_slip = -1.5000089509032453
     # prop_prox = 0.625
     
     
     ## Finding Correct Bifurcation Angle ##
-    a = target_prox_T2 / prox_slip
-    b = target_term_T2 / term_slip
+    a = target_prox_t3 / prox_slip
+    b = target_term_t3 / term_slip
 
     S = np.arccos((1 - a*a - b*b) / (2*a*b))
 
     angle_bot = np.arcsin(a * np.sin(S))
     angle_top = S - angle_bot
 
-    T2_diag = np.sin(angle_top)/np.sin(angle_top+angle_bot)
-    T2_lat  = np.sin(angle_bot)/np.sin(angle_top+angle_bot)
+    t3_diag = np.sin(angle_top)/np.sin(angle_top+angle_bot)
+    t3_lat  = np.sin(angle_bot)/np.sin(angle_top+angle_bot)
 
-    return T2_diag, T2_lat
+    return t3_diag, t3_lat
 
 ######### UTILITIES ##########
 def trans(dx,dy,dz): # 3D translation matrix
     trans=np.eye(4) # create identity matrix
     trans[:,3] = np.array([dx,dy,dz,1]) # add translation vector to last column
     return trans
-def rot_y(Q,L): # rotation and translation matrix about y-axis (ad-abduction angle) 
+
+"""[WARNING] The coordinate frame for RAD finger differs from Valero Cuevas where RAD: (adab=Ry, fe=Rz) and Cuevas: (adab=Rz, fe=Rx). Comments and descriptions are relative to RAD 
+        coordinate frame but current code is for Valero Cuevas.
+    When switching between RAD and Cuevas coordinate frames, you must also reflect the length change in dx, dy, and dz where RAD: (dx,dy,dz=L,0,0) and Cuevas: (dx,dy,dz=0,L,0)"""
+
+def fe_trans(Q,L): # rotation and translation matrix about y-axis (ad-abduction angle) 
     c=np.cos(Q); s=np.sin(Q)
-    dx,dy,dz=L,0,0 # translation vector across length of phalange (x)
-    Ry=np.array([  # rotation matrix about y-axis
-        [c,0,s,0], 
-        [0,1,0,0],
-        [-s,0,c,0],
+    dx,dy,dz=0,L,0 # translation vector across length of phalange (x)
+    Rx=np.array([  # rotation matrix about y-axis
+        [1,0,0,0], 
+        [0,c,-s,0],
+        [0,s,c,0],
         [0,0,0,1]], dtype=float)
-    return Ry@trans(dx,dy,dz) # returns transformation matrix
-def rot_z(Q,L): # rotation and translation matrix about z-axis (flexion-extension angle)
+    return Rx @ trans(dx,dy,dz) # returns transformation matrix
+def adab_trans(Q,L): # rotation and translation matrix about z-axis (flexion-extension angle)
     c=np.cos(Q); s=np.sin(Q)
-    dx,dy,dz=L,0,0 # translation vector across length of phalange (x)
+    dx,dy,dz=0,L,0 # translation vector across length of phalange (x)
     Rz=np.array([  # rotation matrix about z-axis
         [c,-s,0,0], 
         [s, c,0,0],
         [0, 0,1,0],
         [0, 0,0,1]], dtype=float)
-    return Rz@trans(dx,dy,dz) # returns transformation matrix
+    return Rz @ trans(dx,dy,dz) # returns transformation matrix
 def transform(Q,L): # transform the world frame to end effector frame given joint angles and link lengths
     # pass num_joints? check if number of link lengths are equal to number of joints?
     Q = np.asarray(Q)
     origin = np.transpose(np.array([0,0,0,1])) # world/global coordinate (x,y,z,1)=(0,0,0,1)
-    # all_trans = [] # list of transformation matrices
+    trans = np.eye(4) # identity matrix to compile all matrix transformations
     if len(Q) > len(L): # if there are more DOF than phalange lengths, assume first joint is ad-abduction and remaining are flexion-extension
-        T = rot_y(Q[0],0) # MCP ad-abduction transformation matrix (0 length relative to origin) 
+        T = adab_trans(Q[0],0) # MCP ad-abduction transformation matrix (0 length relative to origin) 
         Q = Q[1:] # remove first element of Q
-
-        # all_trans.append(T) # add to list of transformation matrices
-        origin = T @ origin # transform origin to new coordinate frame
+        trans = trans @ T # transform origin to new coordinate frame
     for idx in range(len(Q)): # for each joint angle and length, find the transformation matrix
-        T = rot_z(Q[idx],L[idx]) # rotation and translation matrix
-        # all_trans.append(T) # add to list of transformation matrices
-        origin = T @ origin # transform origin (world) to end effector coordinate frame
-    return np.append(origin[:3], np.sum(Q))  # [x, y, z, sum_of_flexion_angles]
+        T = fe_trans(Q[idx],L[idx]) # rotation and translation matrix
+        
+        trans = trans @ T
+    return np.append((trans @ origin)[:3], np.sum(Q)) # [x, y, z, sum_of_flexion_angles]
 
 ########### FINGER #############
+def get_jacobian_sympy():
+    
+    t1, t2, t3, t4 = sp.symbols('t1 t2 t3 t4')
+    L1, L2, L3 = sp.symbols('L1 L2 L3')
+
+    def rot_y_s(q):
+        c, s = sp.cos(q), sp.sin(q)
+        return sp.Matrix([[c,0,s,0],
+                          [0,1,0,0],
+                          [-s,0,c,0],
+                          [0,0,0,1]])
+    def rot_z_s(q):
+        c, s = sp.cos(q), sp.sin(q)
+        return sp.Matrix([[c,-s,0,0],
+                          [s,c,0,0],
+                          [0,0,1,0],
+                          [0,0,0,1]])
+    def rot_x_s(q):
+        c, s = sp.cos(q), sp.sin(q)
+        return sp.Matrix([[1,0,0,0],
+                          [0,c,-s,0],
+                          [0,s,c,0],
+                          [0,0,0,1]])
+    def trans_s(dx, dy, dz):
+        T = sp.eye(4)
+        T[0,3]=dx; T[1,3]=dy; T[2,3]=dz
+        return T
+
+    T = rot_z_s(t1) * rot_x_s(t2)*trans_s(0,L1,0) * rot_x_s(t3)*trans_s(0,L2,0) * rot_x_s(t4)*trans_s(0,L3,0)
+    sp.pprint(T)
+
+    p = T * sp.Matrix([0,0,0,1])  # end effector position
+    p[-1] = t2+t3+t4
+    sp.pprint(sp.simplify(p))
+
+    # Differentiate to get Jacobian
+    theta = [t1, t2, t3, t4]
+    J = sp.Matrix([[sp.diff(p[i], q) for q in theta] for i in range(len(p))])
+    # print(J)
+    J = sp.simplify(J)
+    return(J)
+
 def get_jacobian_at_pose(Q,L):
     # if singularity is detected??? (maybe) collapse the jacobian matrix (aka remove a DOF) and return the new jacobian matrix
     # figure out how to get the function to work with 2d and 3d inputs (1x3 and 1x4 arrays)
     # attribute of joint type....?
     def end_effector(Q): # input to scipy jacobian must ALWAYS be a function
-           return (transform(Q,L)) # get the end effector position [x,y,z] without trailing 1
+        Q = np.asarray(Q)
+
+        if Q.ndim > 1: # scipy jacobian reshapes Q and is very evil, code below uses witchcraft to fix the issue ¯\_ (ツ)_/¯
+            Q_flat = Q.reshape(Q.shape[0], -1)  # collapse all batch dims → (4, k)
+            return np.stack([transform(Q_flat[:, i], L) for i in range(Q_flat.shape[1])], axis=1)
+        
+        return transform(Q, L) # get the end effector position [x,y,z] without trailing 1
     J = jacobian(end_effector,Q) # get the jacobian matrix of the end effector position with respect to the joint angles
     return J.df # return the jacobian matrix
 
@@ -131,6 +182,8 @@ np.set_printoptions(precision=10, formatter={'float_kind':'{:.5f}'.format})
 
 if __name__ == "__main__":
     q_flx = np.radians(np.array([0,45,45,10])); q_int = np.radians(np.array([0,45,10,10])); q_ext = np.radians(np.array([0,10,10,10]))
-    Q = np.array([45,45,10])
-    print(f"########## CUEVAS MODEL #############\n{get_jacobian_at_pose_3(Q, L)} \n\n")
-    print(f"################ TEST MODEL ############## \n {get_jacobian_at_pose(q_flx, L)}")
+    # Q = np.array([45,45,10])
+    Q = np.radians(np.array([0,45,45,10]))
+    print(f"########## CUEVAS MODEL #############\n{get_jacobian_at_pose_3(q_flx, L)} \n\n")
+    print(f"################ TEST MODEL ############## \n {get_jacobian_at_pose(Q,L)}")
+    # sp.pprint(get_jacobian_sympy())
